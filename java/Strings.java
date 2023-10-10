c58c9322a059781af3d2e448f7a5b54cf6a84b29
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-10-10 21:22:50 +0530

    Add files via upload

---
+++ java/Strings.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Strings{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        // // 1-Input
+        // System.out.print("Enter ur first name: ");
+        // String firstname=sc.next();
+        // System.out.print("Enter ur last name: ");
+        // String lastname=sc.next();
+        // // String s1=sc.nextLine();
+        // System.out.println(firstname);
+        // System.out.println(lastname);
+
+        // //2-Concatination
+        // String fullname=firstname+lastname;
+        // System.out.print("Your full name: ");
+        // System.out.println(fullname);
+
+        //length
+        String fullname="Amartya Kaushik";
+        System.out.println(fullname.length());
+
+        //charAt
+        System.out.println(fullname.charAt(8));
+
+        // //3-Compare
+        String s1="Tony";
+        String s2="Tony";
+        // if(s1.compareTo(s2)){
+        //     System.out.println("both are same");
+        // }
+    }
+
+}
