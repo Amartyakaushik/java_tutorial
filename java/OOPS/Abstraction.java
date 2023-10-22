64b16e738208616d01e6a2bceacd64e6b8534250
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-10-22 19:29:23 +0530

    Add files via upload

---
+++ java/OOPS/Abstraction.java
@@ -0,0 +1,27 @@
+abstract class industry{
+    String title;
+    abstract void display();
+}
+
+class manager extends industry{
+    String department;
+
+    public void display(){
+        // this.department=department;
+        // this.title=title;
+        System.out.print("the manager works as ");
+        System.out.print(department);
+        System.out.print("  in ");
+        System.out.println(title);
+
+    }
+}
+
+public class Abstraction {
+    public static void main(String args[]){
+        manager m1=new manager();
+        m1.title="TCS";
+        m1.department="Sales manager";
+        m1.display();
+    }
+}
