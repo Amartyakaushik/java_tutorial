513efbe456fb33190366326460ae9c745cf27614
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-03-14 20:08:47 +0530

    Samay.java

---
+++ Class_Work/Samay.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+class Samay
+{
+	public static void main(String[] args)
+	{
+		LocalTime sam = LocalTime.now();
+		System.out.println(sam);
+	}
+}
+
+// this will print the live time of the default time zone
